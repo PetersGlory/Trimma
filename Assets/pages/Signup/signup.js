186d4fa2201031046f3df69e3d0cd90f80e7a2cb
Peter-Glo <thomasayomide966@gmail.com>
2020-11-29 12:37:58 -0800

    made some changes in the styling and transfer information from frontend to backend using ajax

--- Assets/pages/Signup/signup.js
+++ Assets/pages/Signup/signup.js
@@ -39,3 +39,47 @@ ChoiceEmployer.addEventListener("click", () =>
 ChoiceEmployee.addEventListener("click", () =>
   makeChoice(this.ChoiceEmployee.id)
 );
+
+//Employee's registration section
+regEmployee = () =>{
+  let pass = document.querySelector("#passwordE");
+  let cpass = document.querySelector("#CpasswordE");
+  let age = document.querySelector("#age");
+  let work = document.querySelector("#work");
+  let company = document.querySelector("#companysname");
+  let address = document.querySelector("#addressE");
+  let mail = document.querySelector("#emailE");
+if ( fname !=="" && cpass.value !== "" && pass.value !== "" &&
+   age.value !== "" &&
+   work.value !== "" &&
+   company.value !== "" &&
+   address.value !== "" && mail.value !=="") {
+
+ if (pass.value == cpass.value) {
+   let obj = {"name":fname.value,
+            "company":company.value, 
+            "age":age.value, 
+            "Iwork":work.value, 
+            "add":address.value,
+            "mail":mail.value,
+            "password"pass.value
+          };
+   let dbparam = JSON.stringify(obj);
+  let regE = new XMLHttpRequest();
+  regE.onreadystatechange = ()=>{
+    if (this.readyState==4 && this.status == 200) {
+      myInfo = JSON.parse(this.responseText);
+      document.querySelector("#respondes").innerText = myInfo; 
+    }
+  };
+  regE.open("POST","signupE.php", true);
+  regE.setRequestHeader("Content-type", "pplication/x-www-form-urlencoded");
+  regE.send("x=" + dbparam);
+ } else {
+   document.querySelector(".helppassmatch").innerText = "Password does not Match..";
+ }
+  // console.log(cpass.value + " "+ age.value + " "+work.value +' '+company.value+" "+ address.value+ " "+ mail.value);
+} else {
+  console.log("Error");
+}
+}
---
+++ Assets/pages/Signup/signupE.php
@@ -0,0 +1,4 @@
+<?php 
+require "../config.php";
+header("Content-Type: application/json; charset=UTF-8");
+ ?>
